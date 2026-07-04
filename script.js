2e17d836803d75990e1336d4fec43031c2fe24cf
Ancientmemer <ancientmemer12@gmail.com>
2026-07-04 16:15:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -44,3 +44,29 @@ window.addEventListener("scroll", () => {
     }
 
 });
+
+const fadeElements = document.querySelectorAll(".fade");
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+}, {
+
+    threshold:0.2
+
+});
+
+fadeElements.forEach(element => {
+
+    observer.observe(element);
+
+});
