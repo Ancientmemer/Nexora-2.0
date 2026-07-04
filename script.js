f27ede4cd55b3ef73d9f44ec82f6ae983677728a
Ancientmemer <ancientmemer12@gmail.com>
2026-07-05 01:22:22 +0530

    Add back to top button functionality

--- script.js
+++ script.js
@@ -70,3 +70,31 @@ fadeElements.forEach(element => {
     observer.observe(element);
 
 });
+
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 300){
+
+        backToTop.style.display = "block";
+
+    }else{
+
+        backToTop.style.display = "none";
+
+    }
+
+});
+
+backToTop.addEventListener("click", () => {
+
+    window.scrollTo({
+
+        top:0,
+
+        behavior:"smooth"
+
+    });
+
+});
