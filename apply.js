180ee302deb57d6b62bedf0b633897496dc1a5cf
Ancientmemer <ancientmemer12@gmail.com>
2026-07-06 22:39:44 +0530

    Create apply.js

---
+++ apply.js
@@ -0,0 +1,10 @@
+<form id="applicationForm">
+  <input id="fullName">
+  <input id="phone">
+  <input id="whatsapp">
+  <input id="email">
+  <input id="education">
+  <textarea id="message"></textarea>
+
+  <button type="submit">Apply</button>
+</form>
