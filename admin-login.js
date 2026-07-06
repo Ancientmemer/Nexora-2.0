1bac35e39994ee61186b90d21b2753061a477cbe
Ancientmemer <ancientmemer12@gmail.com>
2026-07-06 23:17:10 +0530

    Implement admin login functionality

---
+++ admin-login.js
@@ -0,0 +1,23 @@
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit",(e)=>{
+
+e.preventDefault();
+
+const username=document.getElementById("username").value;
+
+const password=document.getElementById("password").value;
+
+if(username==="admin" && password==="Nexora@2026"){
+
+localStorage.setItem("adminLoggedIn","true");
+
+window.location.href="admin.html";
+
+}else{
+
+alert("Invalid Username or Password");
+
+}
+
+});
